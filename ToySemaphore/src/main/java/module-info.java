8134b4e06e3f0095f117cfd8e378582104885b6c
Alexia-Claudia-Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2024-05-18 15:57:24 +0300

    Delete ToySemaphore directory

--- ToySemaphore/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.toysemaphore {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.toysemaphore to javafx.fxml;
-    exports com.example.toysemaphore;
-}
