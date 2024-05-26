5634139bf37be39e5ca921bef875c875bc3e0349
Alexia-Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2024-05-26 13:36:19 +0300

    Delete 4thSemester/MPP/Lab4/Backend directory

--- 4thSemester/MPP/Lab4/Backend/src/main/java/com/mpp/Backend/repository/ISeasonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mpp.Backend.repository;
-
-import com.mpp.Backend.model.Season;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ISeasonRepository extends JpaRepository<Season, Integer> { }
