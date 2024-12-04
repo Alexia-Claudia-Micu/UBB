66720773149b9b750089442fce7f59355ea4ec74
Alexia Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2024-12-04 18:25:31 +0200

    Add files via upload

---
+++ 5thSemester/PPD/assignment_4/Lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lab4.Parser;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4;
+class Program
+{
+    static void Main()
+    {
+        var hosts = new List<string> {
+                "www.cs.ubbcluj.ro/~vancea/",
+                "www.cs.ubbcluj.ro/~forest/newton/index.html",
+                "www.cs.ubbcluj.ro/~rlupsa/edu/pdp/index.html"
+            };
+
+        //DirectCallback.Run(hosts);
+        //TasksMechanism.Run(hosts);
+        AsyncTasksMechanism.Run(hosts);
+    }
+}
