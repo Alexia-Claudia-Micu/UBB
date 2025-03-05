8ba93bf006c711ecdc98e589dc209b1006374f44
Alexia Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2025-03-05 16:10:01 +0200

    Add files via upload

---
+++ 6thSemester/NumericalCalculus/lab_1/lab1_ex1.m
@@ -0,0 +1,12 @@
+x=-4:0.01:7.2;
+px=x.^5-5*x.^4-16*x.^3+16*x.^2-17*x+21;
+plot(x,px);
+
+p = [1 -5 -16 16 -17 21];
+p1 = polyval(p, -2.5)
+
+proots = roots(p);
+
+proots = roots(p);
+proots = roots(p)
+
